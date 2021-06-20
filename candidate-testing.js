0d0137c82e437f175463a19e476da477fb1a19a3
SJonesArts <85701152+sjonesarts@users.noreply.github.com>
2021-06-20 01:33:18 +0000

    Got through asking multiple questions- grading not working yet.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,40 +3,55 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ''
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the First American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = ''
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+let candidateAnswers = []
 
-
-function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
-}
+function askForName() {
+  candidateName = input.question("What is your name? ")
+  return candidateName
 
-function askQuestion() {
+}
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-
+function askQuestion() {
+  for (let i = 0; i <= questions.length - 1; i++) {
+    quizAnswer= input.question("Question: " + questions[i])
+    candidateAnswers.push(quizAnswer)
+    console.log (`Your answer: ${quizAnswer} \nCorrect answer: ${correctAnswers[i]}`)
+  }
+  
+return candidateAnswers
 }
 
-function gradeQuiz(candidateAnswers) {
-
+function gradeQuiz() {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.log (candidateAnswers)
+  console.log (correctAnswers)
+  let numberCorrect = 0;
+  for (let i = 0; i <= questions.length - 1; i++) {
+    if (candidateAnswers[i].toLowerCase == correctAnswers[i].toLowerCase()) { 
+  numberCorrect++;
+  }
+  console.log (numberCorrect)
+  }
 
+  let grade = numberCorrect/questions.length * 100
+  console.log (grade)
 
-  let grade;
-  
-
-  return grade;
+  return grade
 }
 
 function runProgram() {
   askForName();
+  console.log(`Hello, ${candidateName}!`)
   // TODO 1.1c: Ask for candidate's name //
   
   askQuestion();
